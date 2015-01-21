82294849b68b1ca8cd64fc788b8331692d4c8c4c
Kyle Kastner <kastnerkyle@gmail.com>
2015-01-21 11:31:36 -0500

    Dataset reconstruction scripts

---
+++ make_dataset.py
@@ -0,0 +1,74 @@
+from scipy.io import wavfile
+import glob
+import copy
+import numpy as np
+import os
+import sys
+from wrap1 import *
+import matplotlib.pyplot as plt
+#from sklearn.decomposition import FactorAnalysis
+from sklearn.decomposition import PCA
+
+if len(sys.argv) < 2 or sys.argv[1] == "-h" or sys.argv[1] == "--help":
+    print("Usage: make_dataset.py <directory_of_wav_files>")
+    quit()
+
+files_dir = sys.argv[1]
+wav_files = glob.glob(os.path.join(files_dir, "*.wav"))
+spectrogram_data = []
+residual_data = []
+f0_data = []
+file_keys = []
+period = 5.0
+opt = pyDioOption(40.0, 700, 2.0, period, 4)
+for n, wav_file in enumerate(wav_files):
+    print("Processing file %i : %s" % (n, wav_file))
+    fs, X = wavfile.read(wav_file)
+    X = X.astype('float64')
+    f0, time_axis = dio(X, fs, period, opt)
+    f0 = stonemask(X, fs, period, time_axis, f0)
+    spectrogram = cheaptrick(X, fs, period, time_axis, f0)
+    residual = platinum(X, fs, period, time_axis, f0, spectrogram)
+    residual[residual < 1e-6] = 1e-6
+    log_spectrogram = np.log(spectrogram)
+    f0_data.append(f0)
+    spectrogram_data.append(log_spectrogram)
+    residual_data.append(residual)
+    file_keys.append(wav_file)
+
+def normalize(X):
+    fixed_std = X.std(axis=0)
+    return ((X - X.mean(axis=0)) / fixed_std,
+            X.mean(axis=0), fixed_std)
+
+log_spectrograms = np.concatenate(spectrogram_data, axis=0)
+residuals = np.concatenate(residual_data, axis=0)
+
+tf_s = PCA(n_components=100)
+tf_s.fit(log_spectrograms)
+tf_r = PCA(n_components=100)
+tf_r.fit(residuals)
+
+mean_s = np.mean(log_spectrograms, axis=0)
+std_s = np.std(log_spectrograms, axis=0)
+mean_r = np.mean(residuals, axis=0)
+std_r = np.std(residuals, axis=0)
+
+for n, (s, r) in enumerate(zip(spectrogram_data, residual_data)):
+    spectrogram_data[n] = tf_s.transform(s)
+    residual_data[n] = tf_r.transform(r)
+
+f0s = np.concatenate(f0_data, axis=0)
+if len(f0s.shape) < 2:
+    f0s = f0s[:, None]
+t1 = np.concatenate(spectrogram_data, axis=0)
+t2 = np.concatenate(residual_data, axis=0)
+t = np.concatenate((f0s, t1, t2), axis=1)
+
+stats_s = np.concatenate((mean_s[:, None], std_s[:, None]), axis=1)
+stats_r = np.concatenate((mean_r[:, None], std_r[:, None]), axis=1)
+np.save("f0_and_factor_data.npy", t)
+np.save("spectrogram_statistics.npy", stats_s)
+np.save("residual_statistics.npy", stats_r)
+np.save("spectrogram_transform_matrix.npy", tf_s.components_)
+np.save("residual_transform_matrix.npy", tf_r.components_)
--- test_noise.py
+++ test_noise.py
@@ -3,6 +3,7 @@ import copy
 import numpy as np
 import os
 from wrap1 import *
+import matplotlib.pyplot as plt
 
 def soundsc(X, copy=True):
     """
@@ -32,6 +33,10 @@ def test_noise(noise_coeff=0.00):
     period = 5.0
     opt = pyDioOption(40.0, 700, 2.0, period, 4)
 
+    if noise_coeff < 1:
+        noise_str = str(noise_coeff).split('.')[-1]
+    else:
+        noise_str = str(noise_coeff).split('.')[0]
     f0, time_axis = dio(x, fs, period, opt)
 
     f0_by_dio = copy.deepcopy(f0)
@@ -39,23 +44,28 @@ def test_noise(noise_coeff=0.00):
     spectrogram = star(x, fs, period, time_axis, f0)
     spectrogram = cheaptrick(x, fs, period, time_axis, f0)
     residual = platinum(x, fs, period, time_axis, f0, spectrogram)
+    old_spectrogram = np.copy(spectrogram)
+    plt.matshow(old_spectrogram, cmap="gray")
+    plt.title("Before %s noise" % noise_str)
+    plt.savefig("before_%s.png" % noise_str)
     random_state = np.random.RandomState(1999)
-    spectrogram += noise_coeff * random_state.randn(*spectrogram.shape)
-    residual += noise_coeff * random_state.randn(*residual.shape)
+    spectrogram += noise_coeff * np.abs(random_state.randn(*spectrogram.shape))
+    residual += noise_coeff * np.abs(random_state.randn(*residual.shape))
     y = synthesis(fs, period, f0, spectrogram, residual, len(x))
-    aperiodicity = aperiodicityratio(x, fs, period, time_axis, f0)
-    aperiodicity += noise_coeff * random_state.randn(*aperiodicity.shape)
-    ya = synthesis_from_aperiodicity(fs, period, f0, spectrogram, aperiodicity, len(x))
-    noise_str = str(noise_coeff).split('.')[-1]
+    ys = synthesis(fs, period, f0, old_spectrogram, residual, len(x))
     wavfile.write("y_%s.wav" % noise_str, fs, soundsc(y))
-    wavfile.write("ya_%s.wav" % noise_str, fs, soundsc(ya))
+    wavfile.write("y_no_noise.wav", fs, soundsc(ys))
+    plt.clf()
+    plt.plot(soundsc(ys), label='orig')
+    plt.plot(soundsc(y), label='noisy', color='red')
+    plt.title("Comparison of time series with %s noise" % noise_str)
+    plt.legend()
+    plt.savefig("comparison_%s.png" % noise_str)
+
+    #aperiodicity = aperiodicityratio(x, fs, period, time_axis, f0)
+    #aperiodicity += noise_coeff * random_state.randn(*aperiodicity.shape)
+    #ya = synthesis_from_aperiodicity(fs, period, f0, spectrogram, aperiodicity, len(x))
+    #wavfile.write("ya_%s.wav" % noise_str, fs, soundsc(ya))
 
 if __name__ == "__main__":
-    test_noise(1e-16)
-    test_noise(1e-14)
-    test_noise(1e-12)
-    test_noise(1e-10)
-    test_noise(1e-8)
-    test_noise(1e-6)
-    test_noise(1e-4)
     test_noise()
