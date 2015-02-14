081557c1bab4587989c7f4c9fe73efb656519215
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2015-02-14 15:30:32 +0900

    Use setuptools instead of distutils. This is more recommended way.
    https://packaging.python.org/en/latest/current.html

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 import numpy
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup, Extension
 from Cython.Distutils import build_ext
 import os
 
